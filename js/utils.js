c2875485ed850e3c8b7a6ff692d4fac8a840164f
seektien <tien@seekasia.com>
2019-03-16 01:23:22 +0800

    added more frontend component

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,15 @@
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+//___________ Slider _____________
+var slider = document.getElementById("myRange");
+var output = document.getElementById("monthly_bill");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  var roof_size = getPolygonArea(drawPolygon(demoPolygonCoord()).getPath());
+  console.log("Roof size: " + roof_size);
+  getCost(billTokWh(this.value, 0, "day"), roof_size);
+}
